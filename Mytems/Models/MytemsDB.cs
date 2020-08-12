0c4e9d1b5b6adc13ad8de474564c2f07a49e4a9b
idaneden <37079533+idaneden@users.noreply.github.com>
2020-08-12 19:37:04 +0300

    Added DB context

---
+++ Mytems/Models/MytemsDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mytems.Models
+{
+    public class MytemsDB: DbContext
+    {
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<Sale> Sale { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
