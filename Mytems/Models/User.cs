9781396e470e7666c2c39cf71e4b01634e78999d
idaneden <37079533+idaneden@users.noreply.github.com>
2020-08-12 18:20:53 +0300

    canceled hashed password no need for security

--- Mytems/Models/User.cs
+++ Mytems/Models/User.cs
@@ -9,7 +9,7 @@ namespace Mytems.Models
     {
         public int UserID { get; set; }
         public string UserName { get; set; }
-        public string HashedPassword { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
 
 
