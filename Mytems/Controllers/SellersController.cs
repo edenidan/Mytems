7f0f6c46130131f165fe8fad87d3d0ada74a2b9b
idaneden <37079533+idaneden@users.noreply.github.com>
2020-08-15 20:09:35 +0300

    create sellers

---
+++ Mytems/Controllers/SellersController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mytems.Models;
+
+namespace Mytems.Controllers
+{
+    public class SellersController : Controller
+    {
+        private MytemsDB db = new MytemsDB();
+
+        // GET: Sellers
+        public ActionResult Index()
+        {
+            return View(db.Sellers.ToList());
+        }
+
+        // GET: Sellers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Users.Find(id) as Seller;
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // GET: Sellers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sellers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Username,Password,FirstName,LastName,PhoneNumber,Location")] Seller seller)
+        {
+            ModelState.Remove("UserID");
+            ModelState.Remove("JoinedAt");
+            ModelState.Remove("rating");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    seller.UserID = 0;
+                    seller.JoinedAt = DateTime.Now;
+                    seller.Rating = null;
+                    db.Sellers.Add(seller);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbEntityValidationException)
+            {
+                ModelState.AddModelError("Username", "This username is already taken.");
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("Username", "This username is already taken.");
+            }
+
+            return View(seller);
+        }
+
+        // GET: Sellers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Users.Find(id) as Seller;
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // POST: Sellers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,Username,Password,JoinedAt,FirstName,LastName,PhoneNumber,Location,Rating")] Seller seller)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seller).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seller);
+        }
+
+        // GET: Sellers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Users.Find(id) as Seller;
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // POST: Sellers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seller seller = db.Users.Find(id) as Seller;
+            db.Users.Remove(seller);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
