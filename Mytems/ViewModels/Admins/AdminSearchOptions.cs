21e8d5e408f20d98a26632beb540652bd045d651
Erez Ben Moshe <erezbmp@gmail.com>
2020-08-24 13:30:12 +0300

    add search for all the models

---
+++ Mytems/ViewModels/Admins/AdminSearchOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Mytems.Models;
+
+namespace Mytems.ViewModels.Admins
+{
+    public class AdminSearchOptions
+    {
+        public string Username { get; set; }
+
+        [Display(Name = "Joined At Minimum")]
+        public DateTime? JoinedAtMin { get; set; }
+        [Display(Name = "Joined At Maximum")]
+        public DateTime? JoinedAtMax { get; set; }
+
+        public IQueryable<Admin> ApplyOn(IQueryable<Admin> admins)
+        {
+            if (Username != null) admins = admins.Where(a => a.Username.StartsWith(Username));
+            if (JoinedAtMin != null) admins = admins.Where(a => a.JoinedAt >= JoinedAtMin);
+            if (JoinedAtMax != null) admins = admins.Where(a => a.JoinedAt <= JoinedAtMax);
+
+            return admins;
+        }
+    }
+}
---
+++ Mytems/ViewModels/Customers/CustomerSearchOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Mytems.Models;
+
+namespace Mytems.ViewModels.Customers
+{
+    public class CustomerSearchOptions
+    {
+        public string Username { get; set; }
+
+        [Display(Name = "Joined At Minimum")]
+        public DateTime? JoinedAtMin { get; set; }
+        [Display(Name = "Joined At Maximum")]
+        public DateTime? JoinedAtMax { get; set; }
+
+        public IQueryable<Customer> ApplyOn(IQueryable<Customer> customers)
+        {
+            if (Username != null) customers = customers.Where(c => c.Username.StartsWith(Username));
+            if (JoinedAtMin != null) customers = customers.Where(c => c.JoinedAt >= JoinedAtMin);
+            if (JoinedAtMax != null) customers = customers.Where(c => c.JoinedAt <= JoinedAtMax);
+
+            return customers;
+        }
+    }
+}
